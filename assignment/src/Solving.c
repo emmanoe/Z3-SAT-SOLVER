7bccbe99047e748d351d166807cab45fad7d8001
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2019-10-28 18:09:16 +0100

    Touch Solving.c + main.c ps:not working yet

---
+++ assignment/src/Solving.c
@@ -0,0 +1,83 @@
+/**
+ * @brief Generates a formula consisting of a variable representing the fact that @p node of graph number @p number is at position @p position of an accepting path.
+ * 
+ * @param ctx The solver context.
+ * @param number The number of the graph.
+ * @param position The position in the path.
+ * @param k The mysterious k from the subject of this assignment.
+ * @param node The node identifier.
+ * @return Z3_ast The formula.
+ */
+Z3_ast getNodeVariable(Z3_context ctx, int number, int position, int k, int node)
+{
+    return 0;
+}
+
+/**
+ * @brief Generates a SAT formula satisfiable if and only if all graphs of @p graphs contain an accepting path of length @p pathLength.
+ * 
+ * @param ctx The solver context.
+ * @param graphs An array of graphs.
+ * @param numGraphs The number of graphs in @p graphs.
+ * @param pathLength The length of the path to check.
+ * @return Z3_ast The formula.
+ */
+Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs, int pathLength)
+{
+    return 0;
+}
+
+/**
+ * @brief Generates a SAT formula satisfiable if and only if all graphs of @p graphs contain an accepting path of common length.
+ * 
+ * @param ctx The solver context.
+ * @param graphs An array of graphs.
+ * @param numGraphs The number of graphs in @p graphs.
+ * @return Z3_ast The formula.
+ */
+Z3_ast graphsToFullFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs)
+{
+    return 0;
+}
+
+/**
+ * @brief Gets the length of the solution from a given model.
+ * 
+ * @param ctx The solver context.
+ * @param model A variable assignment.
+ * @param graphs An array of graphs.
+ * @return int The length of a common simple accepting path in all graphs from @p graphs.
+ */ 
+int getSolutionLengthFromModel(Z3_context ctx, Z3_model model, Graph *graphs)
+{
+    return 0;
+}
+
+/**
+ * @brief Displays the paths of length @p pathLength of each graphs in @p graphs described by @p model.
+ * 
+ * @param ctx The solver context.
+ * @param model A variable assignment.
+ * @param graphs An array of graphs.
+ * @param numGraph The number of graphs in @p graphs.
+ * @param pathLength The length of path.
+ */
+void printPathsFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength)
+{
+    return 0;
+}
+
+/**
+ * @brief Creates the file ("%s-l%d.dot",name,pathLength) representing the solution to the problem described by @p model, or ("result-l%d.dot,pathLength") if name is NULL.
+ * 
+ * @param ctx The solver context.
+ * @param model A variable assignment.
+ * @param graphs An array of graphs.
+ * @param numGraph The number of graphs in @p graphs.
+ * @param pathLength The length of path.
+ * @param name The name of the output file.
+ */
+void createDotFromModel(Z3_context ctx, Z3_model model, Graph *graphs, int numGraph, int pathLength, char* name)
+{
+    return 0;
+}
