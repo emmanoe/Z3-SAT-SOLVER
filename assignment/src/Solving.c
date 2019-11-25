6ad4b9f08427bf4911eefb202309602975e0a293
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2019-11-25 12:57:55 +0100

    4th subformula implemented

--- assignment/src/Solving.c
+++ assignment/src/Solving.c
@@ -35,12 +35,17 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
 {
     // arrays that store literals, clause and subformulas for generate formula to the path formula
     Z3_ast subphi1 [numGraphs];
-    Z3_ast subphi2 [numGraphs]; 
+    Z3_ast subphi2 [numGraphs];
+    Z3_ast subphi3 [numGraphs];
+    //////////////// 
+    Z3_ast c1_tab [numGraphs];
+    //////////////// 
     Z3_ast literalj [pathLength+1]; // verify variable for each path value
     Z3_ast literall [pathLength+1];
     Z3_ast clause [pathLength+1]; // compare each variable 2 by 2 clause
     Z3_ast clause_array [pathLength+1];
     Z3_ast cnf [pathLength+1];
+    int counter = 0;
 
 	
     for (int i = 0; i<numGraphs; i++){
@@ -64,30 +69,65 @@ Z3_ast graphsToPathFormula( Z3_context ctx, Graph *graphs,unsigned int numGraphs
                 }
                 clause_array[j] = Z3_mk_and(ctx, pathLength+1, clause);
             }
-            //for (int cpt=0; cpt<index; cpt++)
-            //   printf("clause_array[%d]: %s \n",cpt,Z3_ast_to_string(ctx,clause_array[cpt]));            
+            
             cnf[q] = Z3_mk_and(ctx,pathLength+1,clause_array);
-            //printf("cnf[%d]: %s \n",q,Z3_ast_to_string(ctx,cnf[q]));   
+
         }
         subphi1[i] = Z3_mk_and(ctx,numGraphs,cnf); 
-        //printf("subphi1[%d]: %s \n",i,Z3_ast_to_string(ctx,subphi1[i]));  
+         
     
-    // subformula phi2 : the path is a valid path (first vertex of path is s_i, last vertex is t_i)
-        Z3_ast s_i = mk_bool_var(ctx,"False"); Z3_ast t_i = mk_bool_var(ctx,"False");
+        // subformula phi2 : the path is a valid path (first vertex of path is s_i, last vertex is t_i)
+        Z3_ast s_i = mk_bool_var(ctx,"s_i"); Z3_ast t_i = mk_bool_var(ctx,"t_i");
         for (int q = 0; q<orderG(graphs[i]); q++){
-            if (isSource(graphs[i], q))
+            if (isSource(graphs[i], q)){
                 s_i = getNodeVariable(ctx,i,0,pathLength,q);
+                printf("s_i\n");}
             if (isTarget(graphs[i], q)){
                 t_i = getNodeVariable(ctx,i,pathLength-1,pathLength,q);
+                printf("t_i\n");
             }
         }
         Z3_ast valid[2] = {s_i, t_i};
         subphi2[i] = Z3_mk_and(ctx, numGraphs, valid);
+
+        // subformula phi3 : Path is not empty and for each vertex v1 , . . . , vn then (vi, vi+1) in E_i for all 0 ≤ i < n  
+        counter=0;
+		Z3_ast not_tab_c4[2];
+		Z3_ast or_tab_c4[pathLength*orderG(graphs[i])*orderG(graphs[i])];
+		for(int u=0; u<orderG(graphs[i]);u++){
+			for(int v=0;v<orderG(graphs[i]);v++){
+				for(int j=0; j<pathLength;j++){
+					if(!isEdge(graphs[i],u,v)){
+						not_tab_c4[0]=Z3_mk_not(ctx,getNodeVariable(ctx,i,j,pathLength,u));
+						not_tab_c4[1]=Z3_mk_not(ctx,getNodeVariable(ctx,i,(j+1),pathLength,v));
+						or_tab_c4[counter]=Z3_mk_or(ctx,2,not_tab_c4);
+						counter++;
+					}
+				}
+			}
+		}
+        subphi3[i]=Z3_mk_and(ctx,counter,or_tab_c4);
+
+        // C1= Any node in path is a vertex of the corresponding graph
+		Z3_ast or_tab_c1[orderG(graphs[i])];
+		Z3_ast and_tab_c1[pathLength+1];
+		for (int j=0; j<= pathLength; j++){
+			for (int u=0; u<orderG(graphs[i]); u++){
+				or_tab_c1[u]=getNodeVariable(ctx,i,j,pathLength,u);
+			}
+			and_tab_c1[j]=Z3_mk_or(ctx, orderG(graphs[i]), or_tab_c1);
+		}
+		
+		c1_tab[i]= Z3_mk_and(ctx,pathLength+1,and_tab_c1);
     }
+    
     Z3_ast phi1 = Z3_mk_and(ctx, numGraphs, subphi1);
     Z3_ast phi2 = Z3_mk_and(ctx, numGraphs, subphi2);
-    Z3_ast phi0[2] = {phi1, phi2};
-    Z3_ast phi = Z3_mk_and(ctx, 2, phi0);
+    Z3_ast phi3 = Z3_mk_and(ctx, numGraphs, subphi3);
+    Z3_ast phi4 = Z3_mk_and(ctx, numGraphs, c1_tab);
+    Z3_ast phi0[4] = {phi1, phi2, phi3, phi4};
+    Z3_ast phi = Z3_mk_and(ctx, 4, phi0);
+
     return phi;
 }
 
--- assignment/src/main.c
+++ assignment/src/main.c
@@ -8,20 +8,38 @@
 
 int main (int argc, char *argv[])
 {
-    Graph graph0 = getGraphFromFile(argv[1]) ;  
-    Graph graph1 = getGraphFromFile(argv[2]) ;
-    Graph *p = (Graph *)malloc(sizeof(Graph)*2);
-    //p[0] = malloc(sizeof(Graph));
-    //p[1] = malloc(sizeof(Graph));
+    Graph graph[argc];
+    int number = 0;
+    bool DisplayFormula = false;
 
-    p[0] = graph0; p[1] = graph1;
+    if(argc>1){
+        for(int i=1; i<argc;i++){
+            char option=*(argv[i]+1);
+            switch (option)
+            {
+                case 'F':
+                    DisplayFormula = true;
+                    break;
+                default:
+                    graph[number] = getGraphFromFile(argv[i]) ;  
+                    number ++;
+                    break;
+            }
+        }
+    }
+
+    Graph *p = (Graph *)malloc(sizeof(Graph)*2);
+    p[0] = graph[0]; p[1] = graph[1];
     Z3_context ctx = makeContext();
     printf("Noe Creating the context. Must be destroyed at the end of the program.\n");
     
     unsigned int numGraphes = 2;
-    unsigned int pathLengt = 3;
-    Z3_ast formula = graphsToPathFormula(ctx, p, numGraphes, pathLengt);
-    printf("Formula is: %s \n",Z3_ast_to_string(ctx,formula));
+    unsigned int pathLength = 3;
+    Z3_ast formula = graphsToPathFormula(ctx, p, numGraphes, pathLength);
+    if(DisplayFormula){
+        printf("Formula is: %s \n",Z3_ast_to_string(ctx,formula));
+    }
+    
     Z3_lbool isSat = isFormulaSat(ctx, formula); 
 
     switch (isSat)
@@ -31,12 +49,34 @@ int main (int argc, char *argv[])
             break;
 
         case Z3_L_UNDEF:
-                printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
+            printf("We don't know if %s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
             break;
 
         case Z3_L_TRUE:
-                printf("%s is satisfiable.\n",Z3_ast_to_string(ctx,formula));
-                break;
+            printf("formula is satisfiable.\n");
+            Z3_model model = getModelFromSatFormula(ctx,formula);
+            
+            printf("Pour un chemin de longueur %d:\n",pathLength);
+
+            for(int i = 0; i < numGraphes; ++i){
+                int index = 0;
+                Graph G = p[i];
+                printf("coucou\n");
+                int * path = (int*)malloc(pathLength*sizeof(int)); // path is the y of the verificator(X,y)
+                for(int j = 0; j < pathLength; ++j){
+                    for(int q = 0; q < orderG(G); ++q){
+                        Z3_ast var = getNodeVariable(ctx,i, j, pathLength, q);
+                            if(valueOfVarInModel(ctx,model,var)){
+                                path[index]=q;
+                                printf("path[%d]: q[%d]\n", index, path[index]);
+                                index++;
+                            }
+                            else printf("var is null\n");
+                        }
+                    }
+                free(path);
+                }
+            break;
         }
     return 0;
 }
